b32d8b7402dbedd3e6e0bee625bbf1005cbe482e
LuK1337 <priv.luk@gmail.com>
2016-02-24 18:30:08 +0100

    Z00T: init: Use /proc/apid to get a variant
    * /proc/cmdline is not readable by default
    
    Change-Id: I4518246e7255cd03aa95dc7de81b30bddb2cc688

--- init/init_Z00T.cpp
+++ init/init_Z00T.cpp
@@ -52,11 +52,11 @@ void init_msm_properties(unsigned long msm_id , unsigned long msm_ver, char *boa
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    FILE *fp = fopen("/proc/cmdline" , "r");
+    FILE *fp = fopen("/proc/apid" , "r");
     fgets(PRJ_ID, sizeof(PRJ_ID), fp);
     pclose(fp);
 
-    if (strstr(PRJ_ID, "PRJ_ID=3")) {
+    if (ISMATCH(PRJ_ID, "3")) {
         property_set("ro.build.product", "Z00T");
         property_set("ro.product.model", "ASUS_Z00UD");
         property_set("ro.product.device", "ASUS_Z00U");
